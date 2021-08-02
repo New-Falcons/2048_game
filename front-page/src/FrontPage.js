4d97662beaadd0bd96e4ed8779572f6f4fd5ba98
bunnie26 <66902288+bunnie26@users.noreply.github.com>
2021-08-02 11:02:41 +0530

    created front page

--- front-page/src/FrontPage.js
+++ front-page/src/FrontPage.js
@@ -1,12 +1,18 @@
-import React from 'react'
+import React from "react";
 
 function FrontPage() {
-    return (
-        <div className="main-body">
-            <h1>2048-Game</h1>
-            <button>PRESS START</button>
-        </div>
-    )
+  return (
+    <div className="main-body">
+      <h1>
+        <span>2</span>
+        <span>0</span>
+        <span>4</span>
+        <span>8</span>
+        <span id = "gg">-Game</span>
+      </h1>
+      <button>PRESS START</button>
+    </div>
+  );
 }
 
-export default FrontPage
+export default FrontPage;
