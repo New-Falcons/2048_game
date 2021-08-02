8ad90427ef5cb5379ae6b8dbb9afcc8f4edf41b3
bunnie26 <66902288+bunnie26@users.noreply.github.com>
2021-08-02 09:40:47 +0530

    created front page

---
+++ front-page/src/App.js
@@ -0,0 +1,28 @@
+import "./App.css";
+import FrontPage from "./FrontPage";
+
+function App() {
+  return (
+    <div className="App">
+      <div class="context">
+        <FrontPage/>
+      </div>
+      <div class="area">
+        <ul class="circles">
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ front-page/src/FrontPage.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function FrontPage() {
+    return (
+        <div className="main-body">
+            <h1>2048-Game</h1>
+            <button>PRESS START</button>
+        </div>
+    )
+}
+
+export default FrontPage
